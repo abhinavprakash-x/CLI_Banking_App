55e47918063a5cc994857971edb3d0da639bf407
abhinavprakash-x <abhinavprakash.13x@gmail.com>
2025-09-19 21:19:36 +0530

    Added Main Menu

--- main.cpp
+++ main.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 int main(){
     //load data from csv
+
+
+
     //CLI Based Login System
+    int repeat = 1, user_choice;
+    do{
+        cout<<"CLI Based Banking Application\n";
+        cout<<"------------------------------\n";
+        cout<<"Main Menu \n";
+        cout<<"1. Login as Administrator \n";
+        cout<<"2. Login as Customer \n";
+        cout<<"3. Exit System \n";
+        cout<<"Choose Your Preferred Option: ";
+        cin>>user_choice;
+        if(user_choice == 1) cout<<"Admin"; //call admin menu
+        else if(user_choice == 2) cout<<"Customer"; //call customer menu
+        else if(user_choice == 3) return 0; //exit program NB: save data before exit
+        else cout<<"Wrong Option Selected";
+    } while(repeat == 1);
+
+
+
     //options
     //update values
     //save csv
